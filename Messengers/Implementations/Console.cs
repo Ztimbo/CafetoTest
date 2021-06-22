9d87c414f350436cf53fe399c0913a0858ae8db0
Dario Herrera <steve_dario@hotmail.com>
2021-06-22 02:06:52 -0500

    Console class updated

--- Messengers/Implementations/Console.cs
+++ Messengers/Implementations/Console.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CafetoTest.Exceptions;
+using CafetoTest.Helpers;
 using CafetoTest.Messengers.Interfaces;
 
 namespace CafetoTest.Messengers.Implementations
@@ -11,7 +13,23 @@ namespace CafetoTest.Messengers.Implementations
     {
         public void SaveMessage(Message message)
         {
+            var completeMessage = $"Message: {message.MessageContent} Message type: {message.MessageType}";
 
+            switch (message.MessageType)
+            {
+                case MessageType.Warning:
+                    completeMessage += $" Warning Type: {message.WarningType}";
+                    break;
+                case MessageType.Error:
+                    completeMessage += $" Error Type: {message.ErrorType}";
+                    break;
+                case MessageType.Message:
+                    break;
+                default:
+                    throw new MessageTypeNotConfiguredException();
+            }
+
+            System.Console.WriteLine(completeMessage);
         }
     }
 }
